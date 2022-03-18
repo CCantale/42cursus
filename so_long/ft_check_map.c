b770e4f25abe11327a41839b0c7ea52d26567f07
Claudio Cantale <ccantale@e3r5p3.42roma.it>
2022-03-18 18:45:01 +0100

    so_long
    checkmap!!

--- so_long/ft_check_map.c
+++ so_long/ft_check_map.c
@@ -6,7 +6,7 @@
 /*   By: ccantale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 16:42:17 by ccantale          #+#    #+#             */
-/*   Updated: 2022/02/23 18:14:02 by ccantale         ###   ########.fr       */
+/*   Updated: 2022/03/18 18:43:46 by ccantale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,56 +20,52 @@ static char **ft_check_walls(char **map)
 
 	len = 0;
 	while (map[0][len])
-	{
-		if (map[0][len] != '1')
-			return (NULL);
-		++len;
-	}
+		if (map[0][len++] != '1')
+			return (error_msg("Map's outline need be all walls"));
 	i = 1;
-	while (map[i])
+	while (map[i] != NULL)
 	{
+		if (ft_strlen(map[i]) != ft_strlen(map[0]))
+			return (error_msg("Map need be rectangular"));
 		if (map[i][0] != '1')
-			return (NULL);
+			return (error_msg("Map's outline need be all walls"));
 		if (map[i][len - 1] != '1')
-			return (NULL);
+			return (error_msg("Map's outline need be all walls"));
 		++i;
 	}
 	j = 0;
 	while (map[i - 1][j])
 		if (map[i - 1][j++] != '1')
-			return (NULL);
+			return (error_msg("Map's outline need be all walls"));
 	return (map);
 }
 
 
 static char	*ft_join(char *s1, char *s2)
 {
-	int		i;
-	int		j;
+	int		len1;
+	int		len2;
 	char	*join;
 
+	len2 = 0;
+	while (s2[len2] && len2 < 2048)
+		++len2;
 	if (!s1)
-		return (s2);
-	join = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	i = 0;
-	while (s1)
 	{
-		join[i] = s1[i];
-		++i;
+		join = malloc(sizeof(char) * (len2 + 1));
+		ft_strlcpy(join, s2, len2 + 1);
+		return (join);
 	}
-	j = 0;
-	while (s2)
-	{
-		join[i + j] = s2[j];
-		++j;
-	}
-	join[i + j] = 0;
+	len1 = ft_strlen(s1);
+	join = malloc(sizeof(char) * (len1 + len2 + 1));
+	ft_strlcpy(join, s1, len1 + 1);
+	ft_strlcpy(join + len1, s2, len2 + 1);
 	free(s1);
 	return (join);
 }
 
 
-static char	*ft_read_map(int fd)
+static char	*ft_read_map(int fd, int *lines)
 {
 	char	buffer[2048];
 	char	*map;
@@ -82,41 +78,64 @@ static char	*ft_read_map(int fd)
 		map = ft_join(map, buffer);
 		len = read(fd, buffer, 2048);
 	}
+	len = 0;
+	while (map[len])
+	{
+		if (len && map[len] == '\n' 
+				&& map[len + 1] != 0 && map[len + 1] != '\n')
+			*lines += 1;
+		++len;
+	}
 	return (map);
 }
 
-static char	*ft_check_ber(char *filename)
+static void	get_map_info(t_game *game, char **map)
 {
-	size_t	len;
+	int	i;
 
-	len = ft_strlen(filename);
-	return (ft_strnstr(filename, ".ber", len));
+	game->map_x = (int)ft_strlen(map[0]);
+	i = 0;
+	while (map[i][game->map_x - 1] == '1')
+		++i;
+	game->map_y = i;
+	game->map = map;
+	game->start = 0;
+	game->player_sand = 0;
 }
 
-char	**ft_check_map(char *path)
+char	**ft_check_map(t_game *game, char *path)
 {
+							int	i;
 	char	**map;
 	char	*str;
 	int		fd;
+	int		lines;
 
-	if (!ft_check_ber(path))
-		return(NULL);
+	if (!ft_strnstr(path, ".ber", ft_strlen(path)))
+		return (error_msg("File extention need be .ber"));
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
-		return (NULL);
-	str = ft_read_map(fd);
+		return (error_msg("Couldn't open file"));
+	str = ft_read_map(fd, &lines);
 	if (!ft_strchr(str, 'C') || !ft_strchr(str, 'E') || !ft_strchr(str, 'P'))
 	{
 		free(str);
-		return (NULL);
+		return (error_msg("Map need be properly filled in"));
 	}
 	map = ft_split(str, '\n');
+	map[lines + 1] = 0;
+							i = 0;
+							while (map[i])
+							{
+								ft_printf("%s\n", map[i]);
+								++i;
+							}
 	free(str);
 	if (!map)
-	{
-		ft_printf("Map error");
-		return (NULL);
-	}
+		return (error_msg("ft_split failed to create the map"));
+	get_map_info(game, map);
+	map[game->map_y] = 0;
+	close(fd);
 	return (ft_check_walls(map));
 }
 	
