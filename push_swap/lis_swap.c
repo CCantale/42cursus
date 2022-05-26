42e4f832bcf222627b57f28474c4f23b36d89eb1
Claudio Cantale <ccantale@e3r3p1.42roma.it>
2022-05-26 18:13:00 +0200

    push_swap
    lis_swap non segfaulta. Ci siamo quasi!!

--- push_swap/lis_swap.c
+++ push_swap/lis_swap.c
@@ -6,7 +6,7 @@
 /*   By: ccantale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 16:20:40 by ccantale          #+#    #+#             */
-/*   Updated: 2022/05/25 17:05:33 by ccantale         ###   ########.fr       */
+/*   Updated: 2022/05/26 18:10:11 by ccantale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ void	back_swap(int *curr_lis, int nbr, int slots, t_lis *lis)
 				lis->listack[lis->lis_nbr][j] = curr_lis[j];
 				--j;
 			}
-			lis->lis_nbr++;
+			lis->lis_nbr += 1;
 			break ;
 		}
 		--i;
@@ -89,15 +89,13 @@ void	dredge_swap(t_lis *lis)
 	{
 		if (lis->listack[i][0] > 0)
 		{
-			j = i + 1;
-			while (j != i)
+			j = 0;
+			while (j < lis->lis_nbr)
 			{
-				if (j >= lis->lis_nbr)
-					j = 0;
-				if (same_swap(lis->listack[i], lis->listack[j]) > 0)
+				if (lis->listack[j][0] > 0 && j != i
+						&& same_swap(lis->listack[i], lis->listack[j]) > 0)
 				{
-					kill_swap(lis->listack[i]);
-					break ;
+					lis->listack[i] = kill_swap(lis->listack[i]);
 				}
 				++j;
 			}
@@ -114,7 +112,7 @@ void	make_swap(int nbr, int slots, t_lis *lis)
 	lis->listack[lis->lis_nbr] = malloc(sizeof(int) * slots + 1);
 	lis->listack[lis->lis_nbr][0] = 1;	
 	lis->listack[lis->lis_nbr][1] = nbr;
-	lis->lis_nbr++;	
+	lis->lis_nbr += 1;	
 }
 
 /* starts a new LIS that only contains the current nbr */
@@ -128,19 +126,34 @@ void	seq_swap(int *stack_a, int slots, t_lis *lis)
 	while (i < slots)
 	{
 		make_swap(stack_a[i], slots, lis);
-		j = i + 1;
-		while (j != i)
+		j = 0;
+		while (j < lis->lis_nbr)
 		{
-			if (j == slots)
-				j = 0;
 			if (stack_a[i] > lis->listack[j][lis->listack[j][0]])
 				add_swap(lis->listack[j], stack_a[i]);
 			else if (stack_a[i] < lis->listack[j][lis->listack[j][0]])
 				back_swap(lis->listack[j], stack_a[i], slots, lis);
-			dredge_swap(lis);
 			++j;
 		}
+		dredge_swap(lis);
 		++i;
+																int	k;
+																int	l;
+																k = 0;
+																ft_printf("\n\nlap #%d\n", i);
+																while (k < lis->lis_nbr)
+																{
+																	ft_printf("\n");
+																	l = 0;
+																	ft_printf("lis #%d  ", k);
+																	while (l <= lis->listack[k][0])
+																	{
+																		ft_printf("%d", lis->listack[k][l]);
+																		++l;
+																	}
+																	++k;
+																}
+
 	}
 }
 
@@ -152,13 +165,38 @@ void	seq_swap(int *stack_a, int slots, t_lis *lis)
  * controlla se ci sono array che sono interamente contenuti in altri array
  * e si eliminano gli array in eccesso */
 
+void	struct_swap(int slots, t_lis *lis)
+{
+	lis->max = 0;
+	lis->lis_nbr = 0;
+	lis->listack = malloc(sizeof(int *) * (slots * 5));
+}
+
+/* initializes the lis struct */ 
+
 void	lis_swap(t_struct *s)
 {
-	t_lis	lis;
+	t_lis	*lis;
 	//int	lis_rev;
+																int	i;
+																int	j;
 
-	ft_memset(lis, 0, sizeof(lis));
-	seq_swap(s->stack_a, s->slots, &lis, 0);
+	lis = malloc(sizeof(t_lis));
+	struct_swap(s->slots, lis);
+	seq_swap(s->stack_a, s->slots, lis);
+																i = 0;
+																while (i < lis->lis_nbr)
+																{
+																	ft_printf("\n");
+																	j = 0;
+																	ft_printf("lis #%d  ", i);
+																	while (j <= lis->listack[i][0])
+																	{
+																		ft_printf("%d", lis->listack[i][j]);
+																		++j;
+																	}
+																	++i;
+																}
 	/*lis_rev = rev_swap(s->stack_a, s->slots, &seq);
 	if (lis_rev > lis)
 	{
@@ -167,7 +205,8 @@ void	lis_swap(t_struct *s)
 	}
 	else*/
 	//		start_swap(s, lis);
-	
+	free(lis->listack);
+	free(lis);
 }
 
 /* applies the LIS sorting algorithm */
