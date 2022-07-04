9dff435927368d4a2579d1723f10829fc201eb3d
Claudio Cantale <ccantale@e3r2p5.42roma.it>
2022-07-04 14:33:25 +0200

    push_swap
    b_swap() fatta in comp_swap.c

--- push_swap/comp_swap.c
+++ push_swap/comp_swap.c
@@ -6,15 +6,86 @@
 /*   By: ccantale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 00:25:29 by ccantale          #+#    #+#             */
-/*   Updated: 2022/07/03 00:50:20 by ccantale         ###   ########.fr       */
+/*   Updated: 2022/07/04 14:33:17 by ccantale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ps.h"
 
-void	comp_swap()
+int	b_swap(int slots_b, int pos_r)
 {
+	int	pos_rr;
 
+	pos_rr = slots_b - pos_r;
+	if (pos_r < pos_rr)
+		return (pos_r);
+	else
+		return (-pos_rr);
+}
+
+/* returns the smallest nbr of moves to get the nbr in the right position
+** in stack_b. if the moves are intended in reverse, the nbr is returned
+** as a negative */
+
+int	ska_swap(t_struct *s, int nbr)
+{
+	int	i;
+	int	pos_r;
+	int	pos_rr;
+
+	i = 0;
+	while (i < s->slots)
+	{
+		if (s->stack_a[i] > nbr)
+		{
+			pos_r = i;
+			break ;
+		}
+		++i;
+	}
+	pos_rr = s->slots - pos_r;
+	if (pos_r < pos_rr)
+		return (pos_r);
+	else
+		return (-pos_rr);
+}
+
+/* returns the smallest nbr of moves to get stack_a in the right position
+** to recieve the current nbr. if the moves are intended in reverse, the 
+** nbr is returned as a negative */
+
+void	score_swap(t_struct *s, int *scores, int pos_b)
+{
+	int	score_a;
+	int	score_b;
+
+	score_a = ska_swap(s, s->tack_b[pos_b]);
+	score_b = b_swap(s->slots_b, pos_b);
+	if (score_a > 0 && score_b > 0)
+		scores[pos_b] = score_a + score_b;
+	else if (score_a < 0 && score_b < 0)
+		scorese[pos_b] = -1 * (score_a - score_b);
+	else if (score_a > 0)
+		alt_swap(s, pos_b, 1);
+	else
+		alt_swap(s, pos_b, -1);
+}
+
+/*finds the score for the current nbr and puts the score in the scores array */
+
+int	*comp_swap(t_struct *s)
+{
+	int	i;
+	int	*scores;
+
+	scores = malloc(sizeof(int) * s->slots_b);
+	i = 0;
+	while (i < s->slots_b)
+	{
+		score_swap(s, scores, i);
+		++i;
+	}
+	return (scores);
 }
 
 /* makes an array that contains a score for each and every nbr in s->stack_b.
