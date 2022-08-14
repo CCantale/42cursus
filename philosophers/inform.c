1c9d6646c79bb5ea3ccf5f12f7363afcf2ec1ea1
Claudio Cantale <ccantale@e3r3p7.42roma.it>
2022-08-14 23:46:04 +0200

    philosophers
    finita phi_atoi()

--- philosophers/inform.c
+++ philosophers/inform.c
@@ -6,7 +6,7 @@
 /*   By: ccantale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 19:59:18 by ccantale          #+#    #+#             */
-/*   Updated: 2022/08/14 21:32:15 by ccantale         ###   ########.fr       */
+/*   Updated: 2022/08/14 23:40:27 by ccantale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 int	inform(t_ime *relativity, char **info)
 {
-	//t_philo	sophos;
-
 	relativity->how_many_men_make_a_crowd = phi_atoi(info[1]);
 	relativity->what_is_death = phi_atoi(info[2]);
 	relativity->we_are_what_we_eat = phi_atoi(info[3]);
@@ -25,5 +23,6 @@ int	inform(t_ime *relativity, char **info)
 			|| relativity->we_are_what_we_eat == -1
 			|| relativity->is_life_a_dream == -1)
 		return (1);
+	//relativity->sophos = inform_philosophers(relativity);
 	return (0);
 }
