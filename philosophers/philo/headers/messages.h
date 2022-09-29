95afa006dacd0ec481bdf9cf38f82ca788703e81
Claudio Cantale <ccantale@e4r6p2.42roma.it>
2022-09-29 16:43:53 +0200

    philosophers
    recuperati file 2 di 4

---
+++ philosophers/philo/headers/messages.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   messages.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccantale <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/14 19:12:32 by ccantale          #+#    #+#             */
+/*   Updated: 2022/09/16 01:25:40 by ccantale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MESSAGES_H
+# define MESSAGES_H
+
+# include "common_knowledge.h"
+# include <stdio.h>
+
+int		phi_atoi(char *info);
+size_t	phi_time(t_ime *relativity);
+void	phi_sleep(t_ime *relativity, size_t be_right_back);
+
+#endif
