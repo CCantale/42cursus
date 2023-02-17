96d9872a862ac8ca71466be96f3accf01739472a
Claudio Cantale <ccantale@e4r13p3.42roma.it>
2023-02-17 18:23:48 +0100

    Cub3D
    Ho cominciato i check, ma ho troppo sonno, sto a fa' schifo

--- Cub3D/check_map.c
+++ Cub3D/check_map.c
@@ -6,7 +6,7 @@
 /*   By: ccantale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 20:18:33 by ccantale          #+#    #+#             */
-/*   Updated: 2023/02/15 18:27:04 by ccantale         ###   ########.fr       */
+/*   Updated: 2023/02/17 18:22:52 by ccantale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,42 @@ char	*open_and_read(char *path)
 	return (ret);
 }
 
+int	check_textures(char *map)
+{
+	size_t	i;
+	short	cardinal_points[4];
+
+	i = 0;
+	while (i < 4)
+		cardinal_points[i++] = 0;
+	i = 0;
+	while (map[i])
+	{
+		if (i != 0 & map[i] != ' ')
+			continue ;
+		if (cub_strncmp(map + i, " NO "))
+		{
+			if (cardinal_points[0] == 1)
+				return (NOT_OK);
+			cardinal_points[0] = 1;
+		}
+		// gli altri punto cardinali
+		++i;
+	}
+	i = 0;
+	while (i < 4)
+		if (cardinal_points[i++] == 0)
+			return (NOT_OK);
+	return (OK);
+	// checcazzo, è lunga in culooooooo
+	// bisogna trovare un altro sistema...
+}
+
 int	check_map(char **map, char *path)
 {
 	
 	*map = open_and_read(path);
-	if (!map)
+	if (!map || check_textures(*map) == NOT_OK || check_borders(*map) == NOT_OK)
 		return (NOT_OK);
-
-	//read
-	//funcs
-	
 	return (OK);
 }
