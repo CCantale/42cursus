e581130e4499ac64f99fbcc05ef5adae5b44b0fc
Claudio Cantale <claudio.cantale93@gmail.com>
2025-01-03 04:06:37 +0100

    Dr Quine
     Don't know why, but there's an automatic newline at the end of Colleen.c when you cat it. Added it to the string printed by the executable. It now works:
     "./Colleen > test; diff Colleen.c test" returns 0
    
     I also added a simple Makefile. Run "make" to generate the executable and "make test" to execute the test.

--- Dr_Quine/Colleen/Colleen.c
+++ Dr_Quine/Colleen/Colleen.c
@@ -2,12 +2,13 @@
 
 /* This is a comment outside the main function */
 void print_source(char *source) {
-    printf(source, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10); // Print the source code, including the format specifier
+    printf(source, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10); // Print the source code, including the format specifier
+    fflush(stdout);
 }
 
 int main() {
     // This is a comment inside the main function
-    char *source = "#include <stdio.h>%c%c/* This is a comment outside the main function */%cvoid print_source(char *source) {%c    printf(source, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10); // Print the source code, including the format specifier%c}%c%cint main() {%c    // This is a comment inside the main function%c    char *source = %c%s%c;%c    print_source(source);%c    return 0;%c}";
+    char *source = "#include <stdio.h>%c%c/* This is a comment outside the main function */%cvoid print_source(char *source) {%c    printf(source, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10); // Print the source code, including the format specifier%c    fflush(stdout);%c}%c%cint main() {%c    // This is a comment inside the main function%c    char *source = %c%s%c;%c    print_source(source);%c    return 0;%c}%c";
     print_source(source);
     return 0;
 }
