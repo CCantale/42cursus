f7541f1a04a18fb967d064d6283b2d197ed3ac53
Claudio Cantale <claudio.cantale93@gmail.com>
2025-01-03 07:28:18 +0100

    Dr Quine
    Improved Grace. Now it's only 1 macro and there is a comment.

--- Dr_Quine/Grace/Grace.c
+++ Dr_Quine/Grace/Grace.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define TYPE	int
-#define CODE	main(){grace();return(0);}
-#define QUINE	imlovinthisproject
+#define MAIN() int main(void){FILE *dest_file=fopen("Grace_kid.c", "w");char code[] = "#include <stdio.h>%c%c#define MAIN() int main(void){FILE *dest_file=fopen(%cGrace_kid.c%c, %cw%c);char code[] = %c%s%c;if(dest_file == NULL){return(1);}fprintf(dest_file, code, 10, 10, 34, 34, 34, 34, 34, code, 34, 10, 10, 10, 10, 47, 47, 10);fclose(dest_file);return(0);}%c%cMAIN()%c%c%c%c This is a comment%c";if(dest_file == NULL){return(1);}fprintf(dest_file, code, 10, 10, 34, 34, 34, 34, 34, code, 34, 10, 10, 10, 10, 47, 47, 10);fclose(dest_file);return(0);}
 
-void	grace(void)
-{
-	FILE	*dest_file = fopen("Grace_kid.c", "w");
-	char	code[] = "#include <stdio.h>%c#include <stdlib.h>%c%c#define TYPE	int%c#define CODE	main(){grace();return(0);}%c#define QUINE	imlovinthisproject%c%cvoid	grace(void)%c{%c	FILE	*dest_file = fopen(%cGrace_kid.c%c, %cw%c);%c	char	code[] = %c%s%c;%c%c	if (dest_file == NULL)%c		return ;%c	fprintf(dest_file, code, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 34, 34, 10, 34, code, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c	fclose(dest_file);%c}%c%cTYPE CODE%c";
+MAIN()
 
-	if (dest_file == NULL)
-		return ;
-	fprintf(dest_file, code, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 34, 34, 10, 34, code, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10);
-	fclose(dest_file);
-}
-
-TYPE CODE
+// This is a comment
