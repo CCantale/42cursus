5ad0e5d133e5455c65bd2ffa1a1ed55b563be849
Claudio Cantale <claudio.cantale93@gmail.com>
2025-01-12 17:30:05 +0100

    Dr Quine
    Converted comments to the right form

--- Dr_Quine/Colleen/Colleen.c
+++ Dr_Quine/Colleen/Colleen.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-/* This is a comment outside the main function */
+/*
+	This is a comment outside the main function
+*/
 void print_source(char *source) {
-    printf(source, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10); // Print the source code, including the format specifier
+    printf(source, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10);
     fflush(stdout);
 }
 
 int main() {
-    // This is a comment inside the main function
-    char *source = "#include <stdio.h>%c%c/* This is a comment outside the main function */%cvoid print_source(char *source) {%c    printf(source, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10); // Print the source code, including the format specifier%c    fflush(stdout);%c}%c%cint main() {%c    // This is a comment inside the main function%c    char *source = %c%s%c;%c    print_source(source);%c    return 0;%c}%c";
+/*
+	This is a comment inside the main function
+*/
+    char *source = "#include <stdio.h>%c%c/*%c	This is a comment outside the main function%c*/%cvoid print_source(char *source) {%c    printf(source, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10);%c    fflush(stdout);%c}%c%cint main() {%c/*%c	This is a comment inside the main function%c*/%c    char *source = %c%s%c;%c    print_source(source);%c    return 0;%c}%c";
     print_source(source);
     return 0;
 }
--- Dr_Quine/Grace/Grace.c
+++ Dr_Quine/Grace/Grace.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-#define MAIN() int main(void){FILE *dest_file=fopen("Grace_kid.c", "w");char code[] = "#include <stdio.h>%c%c#define MAIN() int main(void){FILE *dest_file=fopen(%cGrace_kid.c%c, %cw%c);char code[] = %c%s%c;if(dest_file == NULL){return(1);}fprintf(dest_file, code, 10, 10, 34, 34, 34, 34, 34, code, 34, 10, 10, 10, 10, 47, 47, 10);fclose(dest_file);return(0);}%c%cMAIN()%c%c%c%c This is a comment%c";if(dest_file == NULL){return(1);}fprintf(dest_file, code, 10, 10, 34, 34, 34, 34, 34, code, 34, 10, 10, 10, 10, 47, 47, 10);fclose(dest_file);return(0);}
+#define MAIN() int main(void){FILE *dest_file=fopen("Grace_kid.c", "w");char code[] = "#include <stdio.h>%c%c#define MAIN() int main(void){FILE *dest_file=fopen(%cGrace_kid.c%c, %cw%c);char code[] = %c%s%c;if(dest_file == NULL){return(1);}fprintf(dest_file, code, 10, 10, 34, 34, 34, 34, 34, code, 34, 10, 10, 10, 10, 10, 10, 10);fclose(dest_file);return(0);}%c%cMAIN()%c%c/*%c	This is a comment%c*/%c";if(dest_file == NULL){return(1);}fprintf(dest_file, code, 10, 10, 34, 34, 34, 34, 34, code, 34, 10, 10, 10, 10, 10, 10, 10);fclose(dest_file);return(0);}
 
 MAIN()
 
-// This is a comment
+/*
+	This is a comment
+*/
